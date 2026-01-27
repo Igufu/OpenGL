3c716742838153b13e79083f066be18d0e3a6b70
Igufu <Igufu.CS@gmail.com>
2026-01-27 14:07:55 -0300

    vertex shader

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -2,6 +2,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h> 
 
+const char* vertexShaderSource = "#version 330 core \n"
+"layout (location = 0) in vec3 aPos; \n"
+"void main() {\n"
+"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); \n"
+"}\0";
+
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
@@ -26,11 +34,6 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	
-
-
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL) {
@@ -48,7 +51,31 @@ int main() {
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glViewport(0, 0, 800, 600); // x pos, y pos, width x height
 
-	
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	/*
+	GL_STREAM_DRAW: data set only once and used by gpu at most a few times
+	GL_STATIC_DRAW: data set only once and used many timers
+	GL_DYNAMIC_DRAW: data changed and set many times
+	*/
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	// Vertex shader creation
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
 
 
 
